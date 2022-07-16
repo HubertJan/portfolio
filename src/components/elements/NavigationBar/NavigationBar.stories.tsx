e2462b867695d1dd0827b68a8dbae169cd7294e1
HubertJan <70658889+HubertJan@users.noreply.github.com>
2022-07-16 21:48:49 +0200

    feat: Add Navigationbar background color

--- src/components/elements/NavigationBar/NavigationBar.stories.tsx
+++ src/components/elements/NavigationBar/NavigationBar.stories.tsx
@@ -11,6 +11,7 @@ export default {
 export const Primary = () => {
     return (
         <NavigationBar
+        backgroundColor="transparent"
         />
     )
 };
--- src/components/elements/NavigationBar/NavigationBar.tsx
+++ src/components/elements/NavigationBar/NavigationBar.tsx
@@ -1,5 +1,9 @@
 import { styled } from "goober";
 
+const Background = styled("div") <{ backgroundColorStyle: string }>`
+background-color: ${(props) => props.backgroundColorStyle};
+`
+
 const Container = styled("div")`
     display: flex;
     flex-direction: row;
@@ -8,11 +12,11 @@ const Container = styled("div")`
     padding-top: 16px;
     padding-bottom: 16px;
     border-radius: 0px 0px 4px 4px;
-    background: ${(props)=> props.theme.colors.background};
+    background: ${(props) => props.theme.colors.background};
     box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px rgba(0, 0, 0, 0.12), 0px 1px 3px rgba(0, 0, 0, 0.2);
     gap: 16px;
 `;
 
-export const NavigationBar: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
-    return (<Container>{children}</Container>);
+export const NavigationBar: React.FC<{ backgroundColor: string, children?: React.ReactNode }> = ({ backgroundColor, children }) => {
+    return (<Background backgroundColorStyle={backgroundColor}><Container>{children}</Container></Background>);
 }

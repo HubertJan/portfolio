cb92ac1869b1c454e3b997de16aa946ed58a0ec2
HubertJan <70658889+HubertJan@users.noreply.github.com>
2022-07-16 21:50:25 +0200

    ..

--- src/components/templates/PortfolioProjectTemplate/PortfolioProjectTemplate.stories.tsx
+++ src/components/templates/PortfolioProjectTemplate/PortfolioProjectTemplate.stories.tsx
@@ -16,6 +16,7 @@ export const Primary = () => {
   const theme = useTheme();
   return (
     <PortfolioProjectTemplate
+    behindNavigationBarColor={theme.colors.primaryVariants[500]}
       projectHeader={
         <ProjectHeader
           title="3ncrypt"
